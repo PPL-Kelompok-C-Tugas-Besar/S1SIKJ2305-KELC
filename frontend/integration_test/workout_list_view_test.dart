a535e336670b8251cb19f51164206c1fc164bf62
portannn <kingswata@gmail.com>
2026-06-14 15:16:09 +0700

    test workout list view

--- frontend/integration_test/workout_list_view_test.dart
+++ frontend/integration_test/workout_list_view_test.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:gymbro/screens/catalogue/catalogue_page.dart';
 import 'package:gymbro/utils/palette.dart';
